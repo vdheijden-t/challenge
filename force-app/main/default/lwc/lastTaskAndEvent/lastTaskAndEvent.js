a9aac6a6b0b42d94efd675735b4627c95dcee9e9
andrew <avanderheyden@gmail.com>
2022-01-06 14:57:38 +0000

    Tweak comment

--- force-app/main/default/lwc/lastTaskAndEvent/lastTaskAndEvent.js
+++ force-app/main/default/lwc/lastTaskAndEvent/lastTaskAndEvent.js
@@ -1,5 +1,5 @@
 import {LightningElement} from 'lwc';
 
 export default class LastTaskAndEvent extends LightningElement {
-    //TODO: Implement logic to retrieve last event and last activity date and set on UI.
+    //TODO: Implement logic to retrieve last event and last task date and set on UI.
 }
